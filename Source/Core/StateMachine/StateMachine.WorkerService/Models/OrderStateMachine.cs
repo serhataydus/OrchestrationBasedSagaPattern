fd342081f7c453e64a71fe00adbf0a5282373c8e
serhataydus <serhataydus@hotmail.com>
2022-04-23 12:47:07 +0300

    State Machine Configuration

--- Source/Core/StateMachine/StateMachine.WorkerService/Models/OrderStateMachine.cs
+++ Source/Core/StateMachine/StateMachine.WorkerService/Models/OrderStateMachine.cs
@@ -1,12 +1,34 @@
 ﻿using MassTransit;
+using MessageBroker.Shared.Interfaces;
 using StateMachine.WorkerService.Data.Enities;
 
 namespace StateMachine.WorkerService.Models
 {
     public class OrderStateMachine : MassTransitStateMachine<OrderStateInstanceEntity>
     {
-        protected OrderStateMachine()
+        public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }
+
+        public State OrderCreated { get; private set; }
+
+        public OrderStateMachine()
         {
+            InstanceState(x => x.CurrentState);
+
+            Event(() => OrderCreatedRequestEvent, y => y.CorrelateBy<int>(x => x.OrderId, z => z.Message.OrderId).SelectId(context => Guid.NewGuid()));
+
+            Initially(When(OrderCreatedRequestEvent).Then(context =>
+            {
+                context.Instance.BuyerId = context.Data.BuyerId;
+
+                context.Instance.OrderId = context.Data.OrderId;
+                context.Instance.CreationDate = DateTime.Now;
+
+                context.Instance.CardName = context.Data.Payment.CardName;
+                context.Instance.CardNumber = context.Data.Payment.CardNumber;
+                context.Instance.CVV = context.Data.Payment.CVV;
+                context.Instance.Expiration = context.Data.Payment.Expiration;
+                context.Instance.TotalAmount = context.Data.Payment.TotalAmount;
+            }).Then(context => { Console.WriteLine($"OrderCreatedRequestEvent Before : {context.ToString()}"); }).TransitionTo(OrderCreated).Then(context => { Console.WriteLine($"OrderCreatedRequestEvent After : {context.Instance}"); }));
         }
     }
 }
